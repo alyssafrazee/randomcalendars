83b76c279fe87cc1458e89e3f38bc9e397de9716
Alyssa Frazee <acfrazee@gmail.com>
2013-08-06 13:11:54 -0400

    now ignoring sample.dat and office_hour_ids files

---
+++ delete_events.py
@@ -0,0 +1,110 @@
+#######################
+###### API SETUP ######
+#######################
+
+# setup proper libraries (in virtualenv)
+import gflags
+import httplib2
+import logging
+import os
+import pprint
+import sys
+
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.tools import run
+from datetime import datetime
+from random import shuffle, randint
+from time import sleep
+
+FLAGS = gflags.FLAGS
+# need this file to authenticate your stuff
+CLIENT_SECRETS = 'client_secrets.json'
+
+# Helpful message to display if the CLIENT_SECRETS file is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to download the client_secrets.json file
+and save it at:
+
+   %s
+
+""" % os.path.join(os.path.dirname(__file__), CLIENT_SECRETS)
+
+# to use for authentication
+FLOW = flow_from_clientsecrets(CLIENT_SECRETS,
+  scope=[
+      'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/calendar.readonly',
+    ],
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+
+# command-line arguments
+gflags.DEFINE_enum('logging_level', 'ERROR',
+    ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+    'Set the level of logging detail.')
+
+###########################
+###### end API setup ######
+###########################
+
+
+###########################
+###### main function ######
+###########################
+
+def delete_events(argv):
+  # Let the gflags module process the command-line arguments
+  try:
+    argv = FLAGS(argv)
+  except gflags.FlagsError, e:
+    print '%s\\nUsage: %s ARGS\\n%s' % (e, argv[0], FLAGS)
+    sys.exit(1)
+
+  # Set the logging according to the command-line flag
+  logging.getLogger().setLevel(getattr(logging, FLAGS.logging_level))
+
+  # If the Credentials don't exist or are invalid, run through the native
+  # client flow. The Storage object will ensure that if successful the good
+  # Credentials will get written back to a file.
+  storage = Storage('sample.dat')
+  credentials = storage.get()
+
+  if credentials is None or credentials.invalid:
+    credentials = run(FLOW, storage)
+
+  # Create an httplib2.Http object to handle our HTTP requests and authorize it
+  # with our good Credentials.
+  http = httplib2.Http()
+  http = credentials.authorize(http)
+
+  # make the calendar
+  service = build('calendar', 'v3', http=http)
+
+  # meaty stuff
+  try:
+
+    print "authentication successful"
+    f = open('office_hour_ids','r')
+    ids = [elem.split('\t')[0] for elem in f.readlines()]
+    for event_id in ids:
+    	service.events().delete(calendarId='primary', eventId=event_id).execute()
+
+  except AccessTokenRefreshError:
+    print ("The credentials have been revoked or expired, please re-run"
+      "the application to re-authorize")
+
+
+
+if __name__ == '__main__':
+  delete_events(sys.argv)
+
+
+
+
+
+
